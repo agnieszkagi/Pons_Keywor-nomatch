6a68faf28cdaebbf62185e83706b2c62fba1fc87
agnieszkagi <agn.ginalska@gmail.com>
2020-01-09 22:58:21 +0100

    my first repository for selenium PO

---
+++ Pons_Keyword-nomatch.py
@@ -0,0 +1,19 @@
+#! python3
+# program downloads content from top.pons.me/?dict=frpl
+
+# import modules and defining values
+import requests, os, bs4, datetime
+
+url = "https://top.pons.me/?dict=frpl"
+file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M") + ".txt"
+
+# Download the page and saving the content in txt file
+
+print("Downloading page %s..." % url)
+res = requests.get(url)
+res.raise_for_status()
+
+ponsFile = open(file_name, "wb")
+for chunk in res.iter_content(100000):
+    ponsFile.write(chunk)
+ponsFile.close()
