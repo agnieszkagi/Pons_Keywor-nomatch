83344763878e83091e994e4615aff48015b2d424
agnieszkagi <agn.ginalska@gmail.com>
2020-01-14 22:52:32 +0100

    variable name revision

--- Pons_Keyword-nomatch2.py
+++ Pons_Keyword-nomatch2.py
@@ -10,19 +10,22 @@ import requests, datetime
 url = "https://top.pons.me/?dict=frpl"
 file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M") + ".txt"
 
-# Download the page and saving the content in txt file
+# Download the page and saving the content in the top_list variable
 
 print("Downloading page %s..." % url)
 res = requests.get(url)
 res.raise_for_status()
 
-keyword_list = []
+top_list = []
 
 for chunk in res.iter_content(100000):
-    keyword_list.append(chunk)
+    top_list.append(chunk)
 
 encoding = "utf-8"
-texto = keyword_list[0].decode(encoding)
-nomatch_list = texto.split("<h3>")
+texto = top_list[0].decode(encoding)
+keyword_list = texto.split("<h3>")
+nomatch_list = keyword_list[7:13]
 
-print(nomatch_list[7:13])
+print(nomatch_list)
+
+# print(nomatch_list[7:13])
